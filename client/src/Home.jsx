44ed704367950a6eeefc7e8a528230ad3fe34799
Christopher4113 <ch.lam1328@gmail.com>
2024-06-29 17:55:01 -0400

    new home

---
+++ client/src/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import menu from './assets/menu.jpg';
+function Home() {
+    return (
+        <div className='d-flex justify-content-center align-items-center bg-secondary vh-100' style= {{
+            backgroundImage: `url(${menu})`,
+            backgroundSize: 'cover', 
+            backgroundPosition: 'center'
+        }}>
+
+        </div>
+         
+    )
+}
+
+export default Home;
