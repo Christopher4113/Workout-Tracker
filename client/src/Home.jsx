1f64897206a3264b89c2a0eda0a7fa7ac077ed25
Christopher4113 <ch.lam1328@gmail.com>
2024-07-02 14:09:51 -0400

    worked on the home paged

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -6,18 +6,23 @@ import food from './assets/food.webp';
 
 function Home() {
     const boxStyle = {
-        background: 'rgba(255, 255, 255, 0.8)',
+        background: 'rgba(255, 255, 255, 0.2)',
         borderRadius: '10px',
-        padding: '20px',
+        padding: '100px', // Larger padding for a bigger box
         margin: '10px',
         textAlign: 'center',
-        width: '200px'
+        width: '500px', // Larger width for a bigger box
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center', // Center align the contents
+        justifyContent: 'center' // Center justify the contents
     };
 
     const imgStyle = {
-        width: '100px',
-        height: '100px',
-        objectFit: 'cover'
+        width: '350px', // Larger width for a bigger image
+        height: '350px', // Larger height for a bigger image
+        objectFit: 'cover',
+        marginBottom: '20px' // Space between the image and text
     };
 
     const linkStyle = {
@@ -30,23 +35,33 @@ function Home() {
             backgroundImage: `url(${menu})`,
             backgroundSize: 'cover', 
             backgroundPosition: 'center',
-            flexDirection: 'column'
+            flexDirection: 'column',
+            position: 'relative'
         }}>
-            <h1 style={{ color: 'white', marginBottom: '20px' }}>Fitness Tracker</h1>
-            <div className='d-flex justify-content-around' style={{ width: '100%' }}>
+            <h1 style={{ 
+                color: 'white', 
+                fontSize: '10em', 
+                fontFamily: 'cursive', 
+                position: 'absolute', 
+                top: '10px' 
+            }}>
+                Fitness Tracker
+            </h1>
+
+            <div className='d-flex justify-content-around' style={{ width: '100%', marginTop: '200px' }}>
                 <div style={boxStyle}>
                     <img src={dumbell} alt="Dumbbell" style={imgStyle} />
-                    <p>Dumbbell Workout</p>
+                    <p>Weight Lifting Tracker</p>
                     <a href="#dumbbell" style={linkStyle}>Learn More</a>
                 </div>
                 <div style={boxStyle}>
                     <img src={running} alt="Running" style={imgStyle} />
-                    <p>Running</p>
+                    <p>Endurance Tracker</p>
                     <a href="#running" style={linkStyle}>Learn More</a>
                 </div>
                 <div style={boxStyle}>
                     <img src={food} alt="Food" style={imgStyle} />
-                    <p>Healthy Food</p>
+                    <p>Calorie Tracker</p>
                     <a href="#food" style={linkStyle}>Learn More</a>
                 </div>
             </div>
