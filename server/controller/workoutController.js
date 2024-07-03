f261f2794c6b833feb7156bf61d1e0570cae417d
Christopher4113 <ch.lam1328@gmail.com>
2024-07-03 17:58:15 -0400

    database update

--- server/controller/workoutController.js
+++ server/controller/workoutController.js
@@ -3,8 +3,14 @@ const mongoose = require('mongoose');
 const cors = require("cors");
 const express = require('express');
 
+app.use(cors());
+
+
 app.post('/workout',(req,res) => {
     userModel.add(req.body)
     .then(user => res.json(user))
     .catch(error => res.json(error));
 });
+
+
+
--- server/models/User.js
+++ server/models/User.js
@@ -1,9 +1,18 @@
 const mongoose = require('mongoose')
 
+
+const workoutSchema = new mongoose.Schema({
+    workout: String,
+    sets: Number,
+    weights: [Number],
+    reps: [Number]
+});
+
 const userSchema = new mongoose.Schema({
     name: String,
     email: String,
     password: String
+    //workouts: [workoutSchema]
 })
 
 const userModel = mongoose.model("users",userSchema)
